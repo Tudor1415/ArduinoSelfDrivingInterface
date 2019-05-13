6e2cddb93299f174d842e1b547249c0485bcb874
Tudor1415 <44468967+Tudor1415@users.noreply.github.com>
2019-05-13 22:02:19 +0200

    Finished calib function needed to be finished

--- aiModel/helpers.py
+++ aiModel/helpers.py
@@ -9,20 +9,12 @@ def time2deg(time):
     return 45/1 * time
 
 
-def write2csv(path2file, text):
-
-    csv.register_dialect('myDialect',
-    delimiter = '|',
-    quoting=csv.QUOTE_NONE,
-    skipinitialspace=True)
-
-    with open(path2file, 'w') as f:
-        writer = csv.writer(f, dialect='myDialect')
-        for row in text:
-            writer.writerow(row)
-    print('Finished')
-    f.close()
-
+def write2csv(path2file, Currents, Keys, Calib, Angles, Image_dir):
+    with open(path2file + '_calib' + '.csv', 'w') as f:
+        f.write("{}, {}, {}, {}, {}\n".format('Current', 'Key', 'Time', 'Angles', 'Image_dir'))
+        for item in range(len(Currents)):
+            f.write("{}, {}, {}, {} \n".format(Currents[item], Keys[item], Calib[item], Angles[item]))
+    
 def calibTime(path2file):
     with open(path2file  + '.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
@@ -31,9 +23,7 @@ def calibTime(path2file):
         Calib = []
         Currents = []
         Angles = []
-        Image_dir = []
-        jpgCounter = 0
-
+    
         for row in readCSV:
             Key = row[2]
             Time = row[1]
@@ -62,17 +52,9 @@ def calibTime(path2file):
                 # print(Calib[i])
                 Angles.append(0)
 
-        print(len(Angles))
-        print(Keys)
-        print(len(Currents))
-        print(len(Calib))
-        print(len(Image_dir))
         # print(Calib)
-    with open(path2file + '_calib' + '.csv', 'w') as f:
-        f.write("{}, {}, {}, {}, {}\n".format('Current', 'Key', 'Time', 'Angles', 'Image_dir'))
-        for item in range(len(Currents)):
-            f.write("{}, {}, {}, {}, {} \n".format(Currents[item], Keys[item], Calib[item], Angles[item], Image_dir[item]))
-    return Calib
+
+    return Currents, Keys, Calib, Angles
 
 def claibImage(path2img, path2file):
     img_dir = []
@@ -82,6 +64,11 @@ def claibImage(path2img, path2file):
     Img_time = []
     Key_time = []
     Currents = []
+    for root, dirs, files in os.walk('Data/IMG/'):
+        for file in files:
+            if file.endswith('.jpg'):
+                jpgCounter += 1
+                img_dir.append('{}_Data/IMG/trainingImage_{}.jpg'.format(Img_time[jpgCounter], jpgCounter))
     for i in range(len(img_dir)):
         for pos, char in enumerate(img_dir):
             if char == '$':
@@ -91,13 +78,9 @@ def claibImage(path2img, path2file):
         h = img_dir[index[0]+1:index[0]+3]
         m = img_dir[index[1]+1:index[1]+3]
         s = img_dir[index[2]+1:index[2]+3]
-        Img_time.append('{} {} {}'.format(h, m, s))
+        append = [h, m, s]
+        Img_time.append(append)
 
-    for root, dirs, files in os.walk('Data/IMG/'):
-        for file in files:
-            if file.endswith('.jpg'):
-                jpgCounter += 1
-                Image_dir.append('{}_Data/IMG/trainingImage_{}.jpg'.format(Img_time[jpgCounter], jpgCounter))
     with open(path2file  + '.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
@@ -115,11 +98,20 @@ def claibImage(path2img, path2file):
             s = img_dir[index[2]+1:index[2]+3]
             append = [h, m, s]
             Key_time.append(append)
-    
-    # for i in range(len(Key_time)):
-    #     h =  Key_time[i][0]
-    #     m =  Key_time[i][1]
-    #     s =  Key_time[i][2]
+    clone = Key_time.copy()
+
+    for i in range(len(Key_time)):
+        h =  Key_time[i][0]
+        m =  Key_time[i][1]
+        s =  Key_time[i][2]
+        h1 =  Img_time[i][0]
+        m1 =  Img_time[i][1]
+        s1 =  Img_time[i][2]
+
+        if h1 == h and m1 == m and s1 == s:
+            clone.append(img_dir[i])
+    return clone
+
 
     
 
--- aiModel/pythonCalib.py
+++ aiModel/pythonCalib.py
@@ -1,3 +1,5 @@
-from helpers import *
+from helpers import write2csv, calibTime, claibImage
 
-calibTime('Data/driving_log')
+Currents, Keys, Calib, Angles = calibTime('Data/driving_log')
+Image_dir = claibImage('Data/IMG', 'Data/driving_log')
+write2csv('Data/driving_log', Currents, Keys, Calib, Angles, Image_dir)
