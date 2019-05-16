32b782d7604d162473cdd3eac34c06a219c05772
Tudor1415 <44468967+Tudor1415@users.noreply.github.com>
2019-05-16 21:37:30 +0200

    Update recordDataPandas.py

--- aiModel/recordDataPandas.py
+++ aiModel/recordDataPandas.py
@@ -1,24 +1,25 @@
 import pandas as pd  
+from helpers import *
 data = pd.read_csv("Data/driving_log.csv",
-       names = ['Img', 'Current', 'Time', 'Key',])
+       names = ['Current', 'Time', 'Key',])
 
-Imgs = data['Img']
+# Imgs = data['Img']
 Keys = data['Key']
 Times = data['Time']
 Current = data['Current']
-
-for i in Imgs.count():
-     if j == " 'D'":
+Angles = []
+for j in Keys:
+    if j == 'd':
         value = time2deg(Calib[i])
         Angles.append(value)
         # print(Angles)
-    elif j == " 'A'":
+    elif j == 'a':
         # print(Calib[i])
         Angles.append(-1 * time2deg(Calib[i]))
     else:
         # print(Calib[i])
         Angles.append(0)
-
+print(Angles)
 # print(Imgs.head())
 
   
