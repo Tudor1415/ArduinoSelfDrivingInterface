a4958b76612a9d5e0d6c732cb1015126e913e032
Tudor1415 <44468967+Tudor1415@users.noreply.github.com>
2019-05-16 15:52:51 +0200

    added functions to recordDataPandas

--- aiModel/recordDataPandas.py
+++ aiModel/recordDataPandas.py
@@ -1,16 +1,25 @@
 import pandas as pd  
-data = pd.read_csv("filename.csv", delimeter = ',',
-       names = ['Img', 'Current', 'Time', 'Key' ])
+data = pd.read_csv("Data/driving_log.csv",
+       names = ['Img', 'Current', 'Time', 'Key',])
 
 Imgs = data['Img']
 Keys = data['Key']
 Times = data['Time']
 Current = data['Current']
 
-# for i in range(0, Imgs.count):
-    
+for i in Imgs.count():
+     if j == " 'D'":
+        value = time2deg(Calib[i])
+        Angles.append(value)
+        # print(Angles)
+    elif j == " 'A'":
+        # print(Calib[i])
+        Angles.append(-1 * time2deg(Calib[i]))
+    else:
+        # print(Calib[i])
+        Angles.append(0)
 
-Imgs
+# print(Imgs.head())
 
   
 
